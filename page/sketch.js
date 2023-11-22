e10e0bb9b886c11f263c21edfe1c3654e32a7534
so9010 <so9010sami@gmail.com>
2023-11-22 15:58:48 +0000

    collison detection

--- page/sketch.js
+++ page/sketch.js
@@ -1,8 +1,5 @@
 class obj {
-  constructor(imgPath, imgSoundPath, imgSize, imgPosX, imgPosY) {
-    this.imgSize = imgSize;
-    this.imgPosX = imgPosX;
-    this.imgPosY = imgPosY;
+  constructor(imgPath, imgSoundPath) {
     this.imgSoundPath = imgSoundPath;
     this.imgPath = imgPath;
     this.img = null;
@@ -19,23 +16,38 @@ class obj {
     }
   }
 
-  setUp() {
+  setUp(imgPosX, imgPosY, imgSize) {
+    this.imgSize = imgSize;
+    this.imgPosX = imgPosX;
+    this.imgPosY = imgPosY;
     this.sound.loop();
     this.analyzer = new p5.Amplitude();
     this.analyzer.setInput(this.sound);
   }
 
   draw() {
-    let distanceFromImage = 1-(dist(mouseX, mouseY, this.imgPosX, this.imgPosY)/(dist(0, 0, this.imgPosX, this.imgPosY)))*1.5; // this needs to be changed inorder to not only go from the centre
-
+    let distanceFromImage = (1 - dist(mouseX, mouseY, this.imgPosX, this.imgPosY) / (this.imgSize * 3));
     if(distanceFromImage < 0){
       distanceFromImage = 0.01; 
+    }
+    else if(distanceFromImage > 0.9){
+      distanceFromImage = 0.9;
     }
 
     let rms = this.analyzer.getLevel();
     this.sound.amp(distanceFromImage);
+
     imageMode(CENTER);
     image(this.img, this.imgPosX, this.imgPosY, this.imgSize + rms * 1000, this.imgSize + rms * 1000);
+  }
+
+  isMouseOn(){
+    if (mouseX >= this.posX && mouseX <= this.posX + this.imgSize && mouseY >= this.posY && mouseY <= this.posY + this.imgSize) {
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
 
@@ -47,7 +59,7 @@ let img1;
 function preload() {
   soundFormats('mp3', 'ogg');
   centreCircleSound = loadSound('../assets/audio/bombabom.mp3');
-  img1 = new obj('../assets/images/img1.jpg', '../assets/audio/audio1.mp3', 100, 100, 100);
+  img1 = new obj('../assets/images/img1.jpg', '../assets/audio/audio1.mp3');
   img1.preLoad();
 }
 
@@ -56,14 +68,10 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   audioCtx = getAudioContext();
   audioCtx.suspend();  
-
-  img1.setUp();
+  img1.setUp(random(width), random(height), width/6);
   ccStartUp();
-
   background(20);
 }
-
-
 
 function draw() {
   // put drawing code here
@@ -102,11 +110,12 @@ function centreCircle() {
   if(size < 0.05){
     size = 0.05;
   }
+  else if(size > 0.9){
+    size = 0.9;
+  }
+
   let rms = analyzer.getLevel();
   centreCircleSound.amp(size);
-  if(size > width/4 - 100) {
-    centreCircleSound.amp(0);
-  }
 
   fill('#b3b3b3');
   noStroke();
@@ -119,3 +128,4 @@ function centreCircle() {
 
 // We want to have a new function that creates a new analyzer, and then effects the images in the same way to how to balls weermade.
 
+// IDEA make it so if the iamge is clicked on, it centres it and blurs out the background.s
